d8d3f9483cd43246c090f00fd1d3c359ac7f9f64
Dejdesboisjolis <jeremyb.lebon@gmail.com>
2023-02-10 15:35:44 +0100

    fixup

--- ExecMerge.bat
+++ ExecMerge.bat
@@ -1,6 +1,6 @@
 @echo off
 rem python PdfMerger.py -p %*
-set /P choice= Taper 1-Merger des pdfs, 2-Extraire des pages d'un pdf, 3-Supprimer des pages d'un pdf: 
+set /P choice=Taper 1-Merger des pdfs 2-Extraire des pages d'un pdf 3-Supprimer des pages d'un pdf: 
 :switch-case
 
   :: Call and mask out invalid call targets
@@ -11,22 +11,20 @@ set /P choice= Taper 1-Merger des pdfs, 2-Extraire des pages d'un pdf, 3-Supprim
   goto :switch-case-end
 
 :switch-case-N-1
-    echo tu as tape %choice% %~1
     START %cd%\PdfMerger.exe -p %* -o m
     goto :switch-case-end 
 
 :switch-case-N-2
-    echo tu as tape %choice% %~1
-    SET /P page= Tapez le numero des pages que vous souhaitez extraire, suivi d'un espace entre chaque:
-    echo tu as tape %page%
-    START %cd%\PdfMerger.exe -p %~1 -o e -pg %page%
+    SET /P selection=Mode de selection pg -Selectionner une ou plusieurs page ------ rg -Selectionner une plage de page (format :min max):
+    echo %selection%
+    SET /P page=Tapez le numero des pages que vous souhaitez extraire, suivi d'un espace entre chaque:
+    START %cd%\PdfMerger.exe -p %~1 -o e -%selection% %page%
     goto :switch-case-end 
 
 :switch-case-N-3
-    echo tu as tape %choice% %~1
-    SET /P page= Tapez le numero des pages que vous souhaitez supprimer, suivi d'un espace entre chaque: 
-    echo tu as tape %page%
-    START %cd%\PdfMerger.exe -p %~1 -o d -pg %page%
+    SET /P selection=Mode de selection 1-Selectionner une ou plusieurs page 2-Selectionner une plage de page (format :min max):
+    SET /P page=Tapez le numero des pages que vous souhaitez supprimer, suivi d'un espace entre chaque: 
+    START %cd%\PdfMerger.exe -p %~1 -o d -%selection% %page%
     goto :switch-case-end
 
 :switch-case-end
